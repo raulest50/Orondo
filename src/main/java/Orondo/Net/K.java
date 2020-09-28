d6ea44e39a64f9c853375975ccd4b41932fff96c
Raul Alzate <raulest50@gmail.com>
2020-09-28 09:56:16 -0500

    se agrego atributo a producto para permitir la agrupacion de los mismos deacuerdo a criterios definibles por el usuario. los respectivos cambios en la interfaz grafica de codificacion y modificacion fueron realizados.

---
+++ src/main/java/Orondo/Net/K.java
@@ -0,0 +1,26 @@
+package Orondo.Net;
+
+/**
+ *
+ * @author Raul Alzate
+ * 
+ * Clase para constantes 
+ */
+public class K {
+    
+    public static final String Key_proposito = "proposito";
+    
+    public static final String Key_body = "body";
+    
+    public static final String PROPOSITO_ID = "IDENTIFICARSE";
+    
+    public static final String ORONDO_ORIG = "ORONDO";
+    
+    public static final String TABLET_ORIG = "IRSUM";
+    
+    public static final String BROWSER_ORIG = "BROW";
+    
+    
+    
+    
+}
